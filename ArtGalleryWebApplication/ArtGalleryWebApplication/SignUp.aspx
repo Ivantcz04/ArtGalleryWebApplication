71cafd5c167dde09ff986c93e35eceb74a773591
Ivantcz04 <ivan.tcz12@gmail.com>
2017-11-16 02:30:50 +0800

    d

--- ArtGalleryWebApplication/ArtGalleryWebApplication/SignUp.aspx
+++ ArtGalleryWebApplication/ArtGalleryWebApplication/SignUp.aspx
@@ -10,6 +10,8 @@
     <form id="form1" runat="server">
         <div>
             <asp:CheckBox ID="CheckBox1" runat="server" />
+            <asp:CheckBoxList ID="CheckBoxList1" runat="server">
+            </asp:CheckBoxList>
         </div>
     </form>
 </body>
--- ArtGalleryWebApplication/ArtGalleryWebApplication/SignUp.aspx.designer.cs
+++ ArtGalleryWebApplication/ArtGalleryWebApplication/SignUp.aspx.designer.cs
@@ -29,5 +29,14 @@ namespace ArtGalleryWebApplication {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.CheckBox CheckBox1;
+        
+        /// <summary>
+        /// CheckBoxList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBoxList CheckBoxList1;
     }
 }
